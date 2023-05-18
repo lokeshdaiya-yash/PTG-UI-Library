2beab711565204bcd8de8c8a7a0c7a49325d2706
Prathmesh Rajendra Bhosale <prathmesh.bhosale@yash.com>
2023-05-18 17:52:49 +0530

    updated file

---
+++ libs/ptg-ui-angular-lib/accordian/accordian/accordian.stories.ts
@@ -0,0 +1,56 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { AccordianComponent } from './accordian.component';
+import { AccordianModule } from '../accordian.module'
+import { ACCORDIAN_DATA } from '@ptg-angular-app/mock/mocks';
+
+export default {
+  title: 'Component/AccordianComponent',
+  component: AccordianComponent,
+
+  decorators: [
+    moduleMetadata({
+      imports: [AccordianModule],
+    }),
+  ],
+} as Meta<AccordianComponent>;
+
+const Template: Story<AccordianComponent> = (args: AccordianComponent) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  isAnimated: false,
+  isDisabled: false,
+  oneAtATime: false,
+  listData : ACCORDIAN_DATA
+};
+
+export const IsAnimated = Template.bind({});
+IsAnimated.args = {
+  isAnimated: true,
+  isDisabled: false,
+  oneAtATime: false,
+  listData : ACCORDIAN_DATA
+};
+
+
+export const IsDisabled = Template.bind({});
+IsDisabled.args = {
+  isAnimated: false,
+  isDisabled: true,
+  oneAtATime: false,
+  listData : ACCORDIAN_DATA
+};
+
+
+export const OneAtATime = Template.bind({});
+OneAtATime.args = {
+  isAnimated: false,
+  isDisabled: false,
+  oneAtATime: true,
+  listData : ACCORDIAN_DATA
+};
+
+
+
