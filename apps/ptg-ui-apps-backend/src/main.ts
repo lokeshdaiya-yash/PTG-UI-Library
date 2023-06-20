d688f7bf8839fa41ab4da84fc67587713daa11ab
surabhidhingra29 <surabhi.dhingra@yash.com>
2023-06-20 14:49:56 +0530

    Code for Google Map Project Migration

--- apps/ptg-ui-apps-backend/src/main.ts
+++ apps/ptg-ui-apps-backend/src/main.ts
@@ -13,12 +13,18 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 app.all('/*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Key, Authorization')
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE, PATCH')
-  next()
-}) //
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Key, Authorization'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'POST, GET, OPTIONS, PUT, DELETE, PATCH'
+  );
+  next();
+}); //
 
 // allow static content to render
 
@@ -27,37 +33,33 @@ app.all('/*', function (req, res, next) {
 app.get('/api', (req: any, res: any) => {
   res.json({ message: 'Welcome to interview-screening-backend!' });
 });
-app.use(express.static(path.join(__dirname, 'assets'))); 
-
-
+app.use(express.static(path.join(__dirname, 'assets')));
 
 let server = app.listen(port, async () => {
   console.log('listening on', port);
 });
 
 // using authentication routes
-app.use('/',router)
-app.use('/',userRoutes)
-app.use('/files',express.static('assets'))
-app.use(express.static(path.join(__dirname, 'assets'))); 
+app.use('/', router);
+app.use('/', userRoutes);
+app.use('/files', express.static('assets'));
+app.use(express.static(path.join(__dirname, 'assets')));
 
-
-const username = encodeURIComponent("priyanshu");
-const password = encodeURIComponent("priyanshu921");
-const dbName = "test"
+const username = encodeURIComponent('priyanshu');
+const password = encodeURIComponent('priyanshu921');
+const dbName = 'test';
 mongoose
   .connect(
     `mongodb+srv://${username}:${password}@naruto.sf8tp46.mongodb.net/${dbName}?retryWrites=true&w=majority`
     // `mongodb+srv://${username}:${password}@atlascluster.27xfa.mongodb.net/${dbName}?retryWrites=true&w=majority`
   )
   .then((data) => {
-    console.log("Successfully connect to MongoDB.");    
-   
+    console.log('Successfully connect to MongoDB.');
   })
   .catch((err) => console.log(err));
 
-  // fallback when refreshed browser
-  app.use('/', express.static(path.join(__dirname, 'public')));
+// fallback when refreshed browser
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.get('*', (req: express.Request, res: express.Response) => {
   const pathname = url.parse(req.url).pathname;
   const pathArr = pathname.split('/');
@@ -65,6 +67,7 @@ app.get('*', (req: express.Request, res: express.Response) => {
     'ptg-angular-app': 'ptg-angular-app',
     'ptg-react-app': 'ptg-react-app',
     'ptg-angular-doc-process': 'ptg-angular-doc-process',
+    'ptg-angular-google-map': 'ptg-angular-google-map',
   };
   const projectName = pathArr[1] && apps[pathArr[1]];
   if (projectName) {
