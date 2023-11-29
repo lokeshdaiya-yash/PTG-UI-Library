20640953247a9d214a34ef9d011e160e873d7407
Nimish Jain <nimish.jain@yash.com>
2023-11-29 17:26:29 +0530

    Created a separate directive to highlight the code snippet

--- apps/ptg-angular-app/src/app/dashboard/toggle-code/toggle-code.component.ts
+++ apps/ptg-angular-app/src/app/dashboard/toggle-code/toggle-code.component.ts
@@ -1,22 +1,13 @@
-import { Component, Input, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-
-declare let hljs: any;
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: "ptg-ui-toggle-code",
   templateUrl: './toggle-code.component.html',
   styleUrls: ['./toggle-code.component.scss']
 })
-export class ToggleCodeComponent implements AfterViewInit {
-  @ViewChild('htmlCodeBlock') htmlCodeBlock!: ElementRef;
-  @ViewChild('typeScriptCode') tsElement!: ElementRef;
+export class ToggleCodeComponent {
   @Input() htmlCode = '';
   @Input() tsCode = '';
 
   isCollapsed = false;
-
-  ngAfterViewInit() {
-    hljs.highlightBlock(this.htmlCodeBlock.nativeElement);
-    hljs.highlightBlock(this.tsElement.nativeElement);
-  }
 }
