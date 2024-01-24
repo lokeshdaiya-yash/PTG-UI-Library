f01b74e3ab6ebfada866e01fca1d05a5f9c7f4ed
Nimish Jain <nimish.jain@yash.com>
2024-01-24 16:51:14 +0530

    Fixed warning for stencil component and tried to fix build

--- libs/ptg-ui-web-components/build/esm/ptg-radiobuton.entry.js
+++ libs/ptg-ui-web-components/build/esm/ptg-radiobuton.entry.js
@@ -6,13 +6,13 @@ const PtgRadiobuton = class {
   constructor(hostRef) {
     registerInstance(this, hostRef);
     this.type = undefined;
-    this.id = undefined;
+    this.ids = undefined;
     this.name = undefined;
     this.value = undefined;
     this.sender = undefined;
   }
   render() {
-    return (h("div", null, h("label", null, h("input", { type: this.type, id: this.id, name: this.name, value: this.value }, " "), " ", this.sender)));
+    return (h("div", null, h("label", null, h("input", { type: this.type, id: this.ids, name: this.name, value: this.value }, " "), " ", this.sender)));
   }
 };
 PtgRadiobuton.style = ptgRadiobuttonCss;
