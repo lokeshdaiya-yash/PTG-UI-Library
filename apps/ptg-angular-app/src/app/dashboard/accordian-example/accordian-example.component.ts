9456ab6674aa5f395b0761e3469dcdc7d7d66c58
chaitalimadde <madde.chaitali@yash.com>
2024-03-01 12:06:30 +0530

    **feat** : added var instead of color codes & propname changed **scope**: changed color prop to bgcolor and used global var instead of hardcoded color & added new color into global.css

--- apps/ptg-angular-app/src/app/dashboard/accordian-example/accordian-example.component.ts
+++ apps/ptg-angular-app/src/app/dashboard/accordian-example/accordian-example.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class AccordianExampleComponent{
   label = 'Bacon Ipsum';
   description = 'Bacon ipsum dolor amet pork chop sausage turkey spare ribs ham hock cupim pork loin capicola bacon ham filet mignon prosciutto boudin turducken. Shank corned beef burgdoggen jowl ribeye. Ham pork pastrami rump meatball buffalo venison andouille picanha fatback pork loin. Venison doner porchetta, chicken leberkas fatback burgdoggen ham andouille landjaeger alcatra. Pork belly pork jerky prosciutto leberkas tail salami tongue frankfurter turducken short loin flank.';
-  color ='white';
+  bgColor ='white';
   defaultOpened = false;
 
   htmlCode = `
-  <ptg-accordion [label]="label" [description]="description" [color]="color" [defaultOpened]="defaultOpened">`;
+  <ptg-accordion [label]="label" [description]="description" [bgColor]="bgColor" [defaultOpened]="defaultOpened">`;
 
   tsCode = `
     import { Component } from '@angular/core';
@@ -24,7 +24,7 @@ export class AccordianExampleComponent{
     export class AccordianExampleComponent {
       label = 'Lorem Ipsum';
       description = 'Lorem ipsum dolor amet pork chop sausage turkey spare ribs ham hock cupim pork loin capicola bacon ham filet mignon prosciutto boudin turducken. Shank corned beef burgdoggen jowl ribeye. Ham pork pastrami rump meatball buffalo venison andouille picanha fatback pork loin. Venison doner porchetta, chicken leberkas fatback burgdoggen ham andouille landjaeger alcatra. Pork belly pork jerky prosciutto leberkas tail salami tongue frankfurter turducken short loin flank.';
-      color ='white';
+      bgColor ='white';
       defaultOpened = false;
     }
   `;
