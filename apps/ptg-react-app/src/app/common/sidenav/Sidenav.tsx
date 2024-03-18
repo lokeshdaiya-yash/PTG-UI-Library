c70d5f7857577b28412a614edf0b21599889094a
Pranjal Joshi <pranjal.joshi@yash.com>
2024-03-18 21:56:50 +0530

    Ui changes and responsiveness

--- apps/ptg-react-app/src/app/common/sidenav/Sidenav.tsx
+++ apps/ptg-react-app/src/app/common/sidenav/Sidenav.tsx
@@ -76,7 +76,7 @@ export function Sidenav() {
                            : 'disabled'
                        }`}
                      >
-                       {/* <i className={`icon ${childItem.icon}`}></i> */}
+                       <i className={`icon child-icon ${childItem.icon}`}></i>
                        <span>{childItem.label}</span>
                     </Link> 
                   </Accordion.Body>
