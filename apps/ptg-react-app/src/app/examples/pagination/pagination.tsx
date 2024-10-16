bcf7fb50de0f75cd9f8793a2afa638162b5dfb36
Manish  Patidar <patidar.manish@Yash.com>
2024-10-16 12:39:54 +0530

    minor change in pagination component

--- apps/ptg-react-app/src/app/examples/pagination/pagination.tsx
+++ apps/ptg-react-app/src/app/examples/pagination/pagination.tsx
@@ -41,51 +41,20 @@ export function PtgPagination(props: PtgPaginationProps) {
     const [pageCount, setPageCount] = useState(3);
     const [pageNumber, setPageNumber] = useState(1);
     
-    const ShowExampleCode = () => {
-      if (!showCode) {
-        setShowCode(true);
-      } else {
-        setShowCode(false);
-      }
-    };
-
     const pageIndex = (num) => {
       setPageNumber(num);
     };
   `;
 
   const htmlCode = `
-    <section className="card-section-two bg-white rounded pt-2 pb-2 mt-2">
-      <div className="row">
-        <div className="col-10 mt-1">
-          <h5 className="font-weight-bold example-heading">Pagination</h5>
-        </div>
-        <div className="col-2 mr-5 mb-2">
-          <CodeIcon
-            onClick={ShowExampleCode}
-            fontSize="large"
-            className="show-code-icon"
-          ></CodeIcon>
-        </div>
-        <hr className="horizontal-line" />
-
-        {showCode && (
-          <ShowCodeComponent
-            componentCode={componentCode}
-            htmlCode={htmlCode}
-            cssCode={cssCode}
-          />
-        )}
-        <div className="ms-2">
-          <PtgUiPagination
-            data={[]}
-            pageCount={pageCount}
-            pageNumber={pageNumber}
-            pageIndex={(num) => pageIndex(num)}
-          />
-        </div>
-      </div>
-    </section>
+    <div className="ms-2">
+      <PtgUiPagination
+        data={[]}
+        pageCount={pageCount}
+        pageNumber={pageNumber}
+        pageIndex={(num) => pageIndex(num)}
+      />
+    </div>
   `;
 
   return (
