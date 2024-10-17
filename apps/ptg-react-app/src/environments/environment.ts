8a2a5dd8c42cee2842e76a25f307587765d87141
Manish  Patidar <patidar.manish@Yash.com>
2024-10-17 15:23:17 +0530

    pagination component integration

--- apps/ptg-react-app/src/environments/environment.ts
+++ apps/ptg-react-app/src/environments/environment.ts
@@ -2,17 +2,18 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 // const baseUrl = 'http://20.114.244.229:1337/';
-//const baseUrl = 'http://localhost:1337/';
-const baseUrl = 'https://yash-ui-strapi.azurewebsites.net/api/';
+const baseUrl = 'http://localhost:1337/';
+//const baseUrl = 'https://yash-ui-strapi.azurewebsites.net/api/';
 export const environment = {
   production: false,
-  baseUrl:baseUrl,
+  baseUrl: baseUrl,
   login: `${baseUrl}auth/local`,
   signup: `${baseUrl}auth/local/register`,
-  msal_postLogoutRedirectUri: "http://localhost:4200",
-  msal_redirectUri: "http://localhost:8080",
-  msal_clientId:"688a4bcc-30fe-44ce-88ca-9e9750714b92",
-  msal_authority:"https://login.microsoftonline.com/yashtechnologies841.onmicrosoft.com",
+  msal_postLogoutRedirectUri: 'http://localhost:4200',
+  msal_redirectUri: 'http://localhost:8080',
+  msal_clientId: '688a4bcc-30fe-44ce-88ca-9e9750714b92',
+  msal_authority:
+    'https://login.microsoftonline.com/yashtechnologies841.onmicrosoft.com',
 };
 
 /*
