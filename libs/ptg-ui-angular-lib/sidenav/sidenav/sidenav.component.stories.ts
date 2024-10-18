dd70f644a8de094f8d6675cf836f257202d6a940
Prathmesh Bhosale <prathmesh.bhosale@yash.com>
2024-10-18 12:43:02 +0530

    storybook build issue resolved

--- libs/ptg-ui-angular-lib/sidenav/sidenav/sidenav.component.stories.ts
+++ libs/ptg-ui-angular-lib/sidenav/sidenav/sidenav.component.stories.ts
@@ -1,31 +1,31 @@
-// import { moduleMetadata, Story, Meta } from '@storybook/angular';
-// import { SidenavComponent } from './sidenav.component';
-// import {SidenavModule} from '../sidenav.module'
-// import { RouterModule } from '@angular/router';
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { SidenavComponent } from './sidenav.component';
+import {SidenavModule} from '../sidenav.module'
+import { RouterModule } from '@angular/router';
 
 
-// export default {
-//   title: 'Component/SidenavComponent',
-//   component: SidenavComponent,
+export default {
+  title: 'Component/SidenavComponent',
+  component: SidenavComponent,
   
-//   decorators: [
-//     moduleMetadata({
-//       imports: [SidenavModule,RouterModule.forRoot([])],
-//     }),
-//   ],
-// } as Meta<SidenavComponent>;
+  decorators: [
+    moduleMetadata({
+      imports: [SidenavModule,RouterModule.forRoot([])],
+    }),
+  ],
+} as Meta<SidenavComponent>;
 
-// const Template: Story<SidenavComponent> = (args: SidenavComponent) => ({
-//   props: args,
-// });
+const Template: Story<SidenavComponent> = (args: SidenavComponent) => ({
+  props: args,
+});
 
-// export const Primary = Template.bind({});
-// Primary.args = {
-//   isMenuOpen: false,
-// };
+export const Primary = Template.bind({});
+Primary.args = {
+  isMenuOpen: false,
+};
 
 
-// export const isMenuOpen = Template.bind({});
-// isMenuOpen.args = {
-//   isMenuOpen: false,
-// };
+export const isMenuOpen = Template.bind({});
+isMenuOpen.args = {
+  isMenuOpen: false,
+};
