66575bc7eab872f554ebe297a1aabeaf90929986
PLondhe <PLondhe@slb.com>
2025-02-10 14:32:49 +0530

    Pipe example snippet

--- apps/ptg-angular-app/src/app/dashboard/pipeexamples/pipeexamples1/pipeexamples1.component.ts
+++ apps/ptg-angular-app/src/app/dashboard/pipeexamples/pipeexamples1/pipeexamples1.component.ts
@@ -24,7 +24,27 @@ export class Pipeexamples1Component {
   value4 = '';
   value5 = '0123456789';
   resources=resources;
-  
+  pipeHtmlCode = `
+ 
+  <p>{{ 'angular pipes' | capitalize }}</p>
+  <p> {{ 123 | currency}}</p>
+  <p>{{ 1234567869 | phoneFormat}}</p>
+  <label aria-label="output" for="numberText"><strong> Capital Pipe:- </strong> {{ pipeTestValue | capitalize}}</label> 
+  <label aria-label="output" for="numberText"><strong> Phone Pipe:- </strong> {{pipeTestValue1 | phoneFormat}}</label>
+  <label aria-label="output" for="numberText"><strong> Currency Pipe:- </strong> {{pipeTestValue3 | currency}}</label>
+  `;
+  pipeTsCode = `
+  import { Component } from '@angular/core';
+ 
+  @Component({
+  selector: 'ptg-ui-pipeexamples1',
+  templateUrl: './pipeexamples1.component.html',
+  styleUrls: ['./pipeexamples1.component.scss']
+  })
+  export class Pipeexamples1Component {
 
-
+  pipeTestValue : string = "hello world";
+  pipeTestValue1 : number = 2394789083;
+  pipeTestValue3 : number = 99;
+  `
 }
